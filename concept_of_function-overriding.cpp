5bbeb4252cd514a948dbc06ab86b5c19cf0225df
Ashis Subedi <73783936+AshisSubedi07@users.noreply.github.com>
2024-02-26 18:12:39 -0800

    Update concept_of_function-overriding.cpp

--- concept_of_function-overriding.cpp
+++ concept_of_function-overriding.cpp
@@ -1,21 +1,21 @@
-//Program to demonstrate the concept of function overriding in c++
+//Program to demonstrate the concept of function overloading in c++
 #include<iostream>
 using namespace std;
 class Parent{ //Creating class
 	public:
-		 virtual void get_data(){ //Function
+		 void get_data(){ //Function
 		cout<<"This is a Parent class: "<<endl;
 		}
 };
 class child:public Parent{
 	public:
-		void get_data() override{
+		void get_data(char a){ //Creating function with argument of type character
 		cout<<"This is a child class: "<<endl;
 		}
 };
  int main(){
  child obj; //Creating object for class child.
- obj.get_data();
+ obj.get_data('A');//Passing arguments
   return 0;
  }
  
